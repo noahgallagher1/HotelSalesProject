8489955fb26a57db7adeb2e5b7a2a49da262c9c9
Noah Gallagher <61672116+noahgallagher1@users.noreply.github.com>
2020-10-20 20:34:25 -0700

    Update UnknownTransactionException.java

--- UnknownTransactionException.java
+++ UnknownTransactionException.java
@@ -1,5 +1,5 @@
 public class UnknownTransactionException extends Exception {
     public UnknownTransactionException(String errorMessage){
-        super(errorMessage + "Unknown Transaction Error");
+        super(errorMessage);
     }
 }
